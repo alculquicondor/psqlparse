bf8df2a1eb1e8ecc88e1ec1b906604ffe4e564e3
Marcos Ferreira <merkkp@gmail.com>
2016-10-15 15:20:41 -0300

    import only parse and psql exception because is the only scope of the test

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,25 +1,26 @@
 import unittest
 
-import psqlparse
+from psqlparse import parse
+from psqlparse.exceptions import PSqlParseError
 
 
 class TestParse(unittest.TestCase):
 
     def test_is_select_statement(self):
         query = "SELECT * FROM my_table"
-        select_stmt = psqlparse.parse(query).pop()
+        select_stmt = parse(query).pop()
         self.assertEqual(select_stmt.type, "SelectStmt")
 
     def test_has_insert_and_select_statement(self):
         query = "INSERT INTO my_table(id) VALUES(1); SELECT * FROM my_table"
-        insert_and_select_stmt = psqlparse.parse(query)
+        insert_and_select_stmt = parse(query)
         stmt_types = [stmt.type for stmt in insert_and_select_stmt]
         self.assertListEqual(stmt_types, ['InsertStmt', 'SelectStmt'])
 
     def test_syntax_error_select_statement(self):
         query = "SELECT * FRO my_table"
         try:
-            psqlparse.parse(query)
+            parse(query)
         except Exception as e:
             exception = e
-        self.assertIsInstance(exception, psqlparse.exceptions.PSqlParseError)
+        self.assertIsInstance(exception, PSqlParseError)
