b0f215bb595d098e2b9c05ec35bc9488976ca39e
Aldo Culquicondor <alculquicondor@gmail.com>
2016-11-24 14:17:10 -0500

    splitting values into their own module

---
+++ psqlparse/nodes/value.py
@@ -0,0 +1,52 @@
+import abc
+
+
+class Value(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __str__(self):
+        return str(self.val)
+
+    @abc.abstractproperty
+    def val(self):
+        pass
+
+
+class Integer(Value):
+
+    def __init__(self, obj):
+        self.ival = obj.get('ival')
+
+    def __int__(self):
+        return self.ival
+
+    @property
+    def val(self):
+        return self.ival
+
+
+class String(Value):
+
+    def __init__(self, obj):
+        self.str = obj.get('str')
+
+    @property
+    def val(self):
+        return self.str
+
+
+class Float(Value):
+
+    def __init__(self, obj):
+        self.str = obj.get('str')
+        self.fval = float(self.str)
+
+    def __float__(self):
+        return self.fval
+
+    @property
+    def val(self):
+        return self.fval
+
+
+
