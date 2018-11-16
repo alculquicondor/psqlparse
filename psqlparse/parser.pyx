799dfbc8094bcfb1c030dad157c459cec1017fc6
Eric Feng <erichfeng@gmail.com>
2018-11-15 22:42:52 -0800

    add normalize method

--- psqlparse/parser.pyx
+++ psqlparse/parser.pyx
@@ -5,7 +5,8 @@ import six
 from .nodes.utils import build_from_obj
 from .exceptions import PSqlParseError
 from .pg_query cimport (pg_query_parse, pg_query_free_parse_result,
-                       PgQueryParseResult)
+                        pg_query_normalize, pg_query_free_normalize_result,
+                       PgQueryParseResult, PgQueryNormalizeResult)
 
 
 def parse_dict(query):
@@ -34,3 +35,26 @@ def parse_dict(query):
 
 def parse(query):
     return [build_from_obj(obj) for obj in parse_dict(query)]
+
+
+def normalize(query):
+    cdef bytes encoded_query
+    cdef PgQueryNormalizeResult result
+
+    if isinstance(query, six.text_type):
+        encoded_query = query.encode('utf8')
+    elif isinstance(query, six.binary_type):
+        encoded_query = query
+    else:
+        encoded_query = six.text_type(query).encode('utf8')
+
+    result = pg_query_normalize(encoded_query)
+    if result.error:
+        error = PSqlParseError(result.error.message.decode('utf8'),
+                               result.error.lineno, result.error.cursorpos)
+        pg_query_free_normalize_result(result)
+        raise error
+
+    normalized_query = result.normalized_query.decode('utf8')
+    pg_query_free_normalize_result(result)
+    return normalized_query
---
+++ test/test_normalize.py
@@ -0,0 +1,11 @@
+import unittest
+
+from psqlparse import normalize
+from psqlparse.exceptions import PSqlParseError
+from psqlparse import nodes
+
+
+class NormalizeTest(unittest.TestCase):
+    def test_simple(self):
+        query = "SELECT * FROM mytable WHERE col1 = 1"
+        assert normalize(query) == "SELECT * FROM mytable WHERE col1 = $1"
